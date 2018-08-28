8b94e4f864b0cc538d16b29fba7b0fb58a32ca0f
Rina Lingad <pixie.czar@gmail.com>
2018-08-28 14:39:23 +1200

    Add callback function for sign-in

--- project.py
+++ project.py
@@ -20,7 +20,7 @@ session = DBSession()
 def showLogin():
     state = ''.join(random.choice(string.ascii_letters + string.digits) for x in xrange(32))
     login_session['state'] = state
-    return render_template('login.html')
+    return render_template('login.html', STATE = state)
 
 
 #JSON APIs to view Restaurant Information
